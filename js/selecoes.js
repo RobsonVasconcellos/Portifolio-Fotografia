ce640f2a783a813614acda04fc56cb3b91b7240a
Robson <robson.ribeiro@wmtrading.com.br>
2022-08-04 09:12:33 -0300

    Portifólio

---
+++ js/selecoes.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+  $("body").click(function(){
+    //Codifique os select (seleções) neste bloco de código
+    $("h1").css("color","yellow");
+    $(".icon-arrow-down2").css("color","yellow");
+    $("#casa").css("color","yellow");
+    $("#especial").css("color","yellow");
+    $("#olamundo").css("color","yellow");
+    $("#getin").css("color","yellow");
+    $(".autores").css("color","yellow");
+    $("#designed").css("color","yellow");
+  });
+});
